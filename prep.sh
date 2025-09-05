2c46000ac7621b37ea63326594a6dce5a4adf6bd
eshaan <eshaan2031@icloud.com>
2025-09-05 10:00:43 -0500

    Update prep.sh

--- prep.sh
+++ prep.sh
@@ -2,4 +2,4 @@ CPPFLAGS="-I$HOME/.local_tcltk/include" \
 LDFLAGS="-L$HOME/.local_tcltk/lib -ltk8.6 -ltcl8.6" \
 PYTHON_CONFIGURE_OPTS="--with-tcltk-includes='-I$HOME/.local_tcltk/include' --with-tcltk-libs='-L$HOME/.local_tcltk/lib'" \
 
-python3.7 -m pip install cefpython3
+~/.pyenv/versions/3.7.17/bin/python3.7 -m pip install cefpython3
